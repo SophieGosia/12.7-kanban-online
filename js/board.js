69a453c272caa956cee92aa3b51671ec716a09e0
Małgorzata Skrudlik <sophie.gosia@gmail.com>
2018-06-06 00:24:04 +0200

    Create functional version of kanban

---
+++ js/board.js
@@ -0,0 +1,40 @@
+var board = {
+  name: 'Kanban board',
+  createColumn: function(column) {
+    this.element.append(column.element);
+    initSortableCards();
+    initSortableColumns();
+  },
+  element: $('#board .column-container')
+};
+
+$('.create-column').click(function () {
+  var columnName = prompt('Enter a column name');
+  $.ajax({
+    url: baseUrl + '/column',
+    method: 'POST',
+    data: {
+      name: columnName
+    },
+    success: function (response) {
+      var column = new Column(response.id, columnName);
+      board.createColumn(column);
+    }
+  });
+});
+
+// Sorting cards
+function initSortableCards() {
+  $('.column-card-list').sortable({
+    connectWith: '.column-card-list',
+    placeholder: 'card-placeholder'
+  }).disableSelection();
+}
+
+// Sorting columns
+function initSortableColumns() {
+  $('.column-container').sortable({
+    connectWith: '.column-container',
+    placeholder: 'column-placeholder'
+  }).disableSelection();
+}
